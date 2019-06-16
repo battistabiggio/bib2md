c0797dda749d2b0e07be8fb670709382294a036e
Cassio Batista <cassio.batista.13@gmail.com>
2019-06-16 15:18:12 -0300

    lots of refacts
    - endnote how handled entirely. tailnote removed
    - field string alignment '%-XXs'
    - title split now pure HTML isntead of mardown bold
    - inproceedings and journal required and optional fields are now handled to
      choose which value goes into the front matter and which does not

--- bib2md.py
+++ bib2md.py
@@ -17,7 +17,33 @@ import bibtexparser
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
 
-YAML_FM_DELIM = '---'
+FM_DELIM = '---' # YAML
+
+# http://bib-it.sourceforge.net/help/fieldsAndEntryTypes.php#proceedings
+BIB_FIELDS = {
+    'article': {
+        'req': ['journal', 'year'],
+        'opt': ['volume', 'number', 'pages', 
+                'month', 'note'],
+    },
+    'inproceedings': {
+        'req': ['booktitle', 'year'],
+        'opt': ['editor', 'volume', 'number', 
+                'series', 'pages', 'address', 
+                'month', 'organization', 'publisher', 'note'],
+    },
+    'booklet':       { 'req': [], 'opt': [], },
+    'inbook':        { 'req': [], 'opt': [], },
+    'incollection':  { 'req': [], 'opt': [], },
+    'misc':          { 'req': [], 'opt': [], },
+    'book':          { 'req': [], 'opt': [], },
+    'proceedings':   { 'req': [], 'opt': [], },
+    'unpublished':   { 'req': [], 'opt': [], },
+    'mastersthesis': { 'req': [], 'opt': [], },
+    'phdthesis':     { 'req': [], 'opt': [], },
+    'techreport':    { 'req': [], 'opt': [], },
+    'manual':        { 'req': [], 'opt': [], },
+}
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='A script to parse a single '\
@@ -27,53 +53,54 @@ if __name__ == "__main__":
                 help='input bibtex .bib file', required=True)
     parser.add_argument('-d', '--dir', metavar='DIR', type=str, 
                 help='output dir to store .md files')
-    
     args = parser.parse_args()
 
+    if args.dir is None:
+        args.dir = '.' 
     if not os.path.isfile(args.input):
         print('File "' + args.input + '" does not exist.')
         sys.exit(1)
-
-    if args.dir is None:
-        args.dir = '.'
 
     plain_backend = find_plugin('pybtex.backends', 'plaintext')
     plain_style   = find_plugin('pybtex.style.formatting', 'plain')()
 
     bib_data = database.parse_file(args.input)
+
+    # make title bold
     for bibkey in bib_data.entries:
         title = bib_data.entries[bibkey].fields['title']
-        bib_data.entries[bibkey].fields['title'] = '**{}**'.format(title)
+        bib_data.entries[bibkey].fields['title'] = '<b>{}</b>'.format(title)
+        # TODO make 'booktitle' and 'journal' fields italic
 
     for plainentry in plain_style.format_bibliography(bib_data):
-        markdown = os.path.join(args.dir, '{}.md'.format(plainentry.key))
-        endnote  = plainentry.text.render(plain_backend('utf8'))
-        authors  = endnote.split('*')[0].split(',')
-        title    = endnote.split('*')[2].strip().rstrip().capitalize()
-        tailnote = endnote.split('*')[4].strip().rstrip()
-        if 'URL:' in tailnote:
-            tailnote = tailnote.split('URL:')[0].rstrip()
+        markdown   = os.path.join(args.dir, '{}.md'.format(plainentry.key))
+        plaintext  = re.split('<b>|</b>|URL:', 
+                    plainentry.text.render(plain_backend('utf8')))
+        endnote  = '{}\'<b>{}</b>\'{}'.format(plaintext[0], 
+                    plaintext[1].capitalize(), plaintext[2].rstrip())
+        authors  = plaintext[0].split(',')
+        title    = plaintext[1].strip().rstrip().capitalize()
         with open(markdown, 'w', encoding='utf8') as md:
-            md.write(YAML_FM_DELIM + '\n')
-            md.write('tailnote: "%s"\n' % tailnote)
-            md.write('title: "%s"\n'    % title.capitalize())
+            md.write(FM_DELIM + '\n')
             md.write('authors:\n')
             for author in authors:
                 md.write('- %s\n' % author.strip().rstrip().rstrip('.').lstrip('and '))
+            md.write('%-10s "%s"\n' % ('title:', title))
+            md.write('%-10s "%s"\n' % ('endnote:', endnote))
 
     for bibentry in bib_data.entries.values():
         markdown = os.path.join(args.dir, '{}.md'.format(bibentry.key))
         with open(markdown, 'a', encoding='utf8') as md:
-            md.write('pub_type: "%s"\n' % bibentry.type)
+            md.write('%-10s "%s"\n' % ('pub_type:', bibentry.type))
             for field, value in bibentry.fields.items():
-                if field != 'title' and field != 'url':
-                    md.write('%s: "%s"\n' % (field, value))
-            md.write('date: "%s-01-01"\n' % bibentry.fields['year'])
-            md.write(YAML_FM_DELIM + '\n\n')
+                if field in BIB_FIELDS[bibentry.type]['req']:
+                    md.write('%-10s "%s"\n' % (field+':', value))
+            md.write('%-10s "%s-01-01"\n' % ('date:', bibentry.fields['year']))
+            md.write(FM_DELIM + '\n\n')
 
             md.write('## Abstract\n')
             if 'abstract' in bibentry.fields:
-                md.write('```' + '\n')
+                md.write('```' + '\n') # FIXME
                 md.write(bibentry.fields['abstract'] + '\n')
                 md.write('```' + '\n')
             else:
@@ -99,6 +126,6 @@ if __name__ == "__main__":
             bibtw.indent = '    '
 
             md.write('## BibTeX Citation\n')
-            md.write('```bibtex' + '\n')
+            md.write('```bibtex' + '\n') # FIXME use shortcote highlight
             md.write(bibtw.write(bibdb))
             md.write('```' + '\n')
